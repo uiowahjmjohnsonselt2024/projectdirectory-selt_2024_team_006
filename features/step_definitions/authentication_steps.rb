f219f4b6635d2259173d58663c5729c472a8a3ad
Nick Hansel <nhansl@uiowa.edu>
2024-11-09 10:47:07 -0600

    devise auth setup

---
+++ features/step_definitions/authentication_steps.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+Given('I am on the sign up page') do
+  visit new_user_registration_path
+end
+
+Given('I am on the sign in page') do
+  visit new_user_session_path
+end
+
+Given('a user exists with email {string} and password {string}') do |email, password|
+  User.create(email: email, password: password, password_confirmation: password)
+end
+
+Given('I am signed in as {string}') do |email|
+  user = User.find_by(email: email)
+  visit new_user_session_path
+  fill_in 'Email', with: user.email
+  fill_in 'Password', with: 'password123'
+  click_button 'Log in'
+end
+
+When('I fill in {string} with {string}') do |field, value|
+  fill_in field, with: value
+end
+
+When('I press {string}') do |button|
+  click_button button
+end
+
+When('I click {string}') do |link|
+  click_button link
+end
+
+Then('I should see {string}') do |content|
+  expect(page).to have_content(content)
+end
