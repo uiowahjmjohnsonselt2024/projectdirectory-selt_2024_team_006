199284ff90f4b7a4c506004d074a10c4e2cca935
Spencer-Holman <spenholman@uiowa.edu>
2024-11-12 15:30:07 -0600

    single player button added

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,6 @@
+class GamesController < ApplicationController
+  def single_player
+    # Add any setup logic for the single-player mode here
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root 'home#index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'single_player', to: 'games#single_player'
   # Defines the root path route ("/")
   # root "articles#index"
 end
