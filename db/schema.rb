3e7179c264721d9bd41314ccaef4b06319a80fe2
Nick Hansel <nhansl@uiowa.edu>
2024-11-14 16:07:17 -0600

    fix conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
 ActiveRecord::Schema[7.0].define(version: 20_241_112_232_841) do
   create_table 'items', force: :cascade do |t|
     t.string 'name'
@@ -8,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 20_241_112_232_841) do
     t.datetime 'created_at', null: false
     t.datetime 'updated_at', null: false
     t.string 'image_url'
-    t.integer 'damage' # Retaining the damage attribute
+    t.integer 'damage'
   end
 
   create_table 'user_items', force: :cascade do |t|
