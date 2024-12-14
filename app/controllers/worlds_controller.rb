c93d5e478f78edf693b4aea930d5f97150a88796
Nick Hansel <holywhale7@gmail.com>
2024-12-14 08:51:08 -0600

    fixed merge conflicts

--- app/controllers/worlds_controller.rb
+++ app/controllers/worlds_controller.rb
@@ -16,6 +16,25 @@ class WorldsController < ApplicationController
     redirect_to game_path(@world)
   end
 
+  def shard_move
+    @world = find_world
+    return redirect_to_single_player('World not found.') unless @world
+
+    player_cell = find_player_cell
+    return redirect_to_single_player('Player not found on grid.') unless player_cell
+
+    x, y = target_coordinates
+    new_cell = find_new_cell([x, y])
+
+    return redirect_to_game('Player is in battle') if player_in_battle?
+
+    return redirect_to_game('Player already at this location') if same_location?(player_cell, x, y)
+
+    return redirect_to_game('Insufficient funds') if insufficient_funds_for_move?
+
+    process_shard_move(player_cell, new_cell)
+  end
+
   def index; end
 
   def acknowledge_encounter
@@ -64,6 +83,42 @@ class WorldsController < ApplicationController
   end
 
   private
+
+  def redirect_to_single_player(alert_message)
+    redirect_to single_player_path, alert: alert_message
+  end
+
+  def redirect_to_game(alert_message)
+    redirect_to game_path(@world), alert: alert_message
+  end
+
+  def insufficient_funds_for_move?
+    current_user.shards_balance < 50
+  end
+
+  def process_shard_move(player_cell, new_cell)
+    if valid_position?([new_cell.x, new_cell.y])
+      process_move(player_cell, new_cell)
+      current_user.decrement!(:shards_balance, 50)
+    end
+    redirect_to game_path(@world)
+  end
+
+  def sufficient_funds_for_move?
+    current_user.shards_balance >= 50
+  end
+
+  def same_location?(player_cell, x_pos, y_pos)
+    player_cell.x == x_pos && player_cell.y == y_pos
+  end
+
+  def player_in_battle?
+    Battle.exists?(player: current_user, world: @world, state: 'active')
+  end
+
+  def target_coordinates
+    [params[:x].to_i, params[:y].to_i]
+  end
 
   def track_achievement_progress(name)
     achievement = Achievement.find_by(name: name)
