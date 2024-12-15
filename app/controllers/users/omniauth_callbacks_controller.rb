83b1bd02f43d4b3f9b6383d3a72e14cc521c8b49
Spencer-Holman <spenholman@uiowa.edu>
2024-12-14 20:04:14 -0600

    linting

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     def google_oauth2
       @user = User.from_omniauth(request.env['omniauth.auth'])
-      
+
       if @user.persisted?
         flash[:notice] = I18n.t 'devise.omniauth_callbacks.success', kind: 'Google'
         sign_in_and_redirect @user, event: :authentication
